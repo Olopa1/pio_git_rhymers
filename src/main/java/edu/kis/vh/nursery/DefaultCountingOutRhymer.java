16b8f0efad9b56b7c73ac7a3c356b8d083172f14
igorgardzielewski <igorgardzielewski3@gmail.com>
2024-05-09 11:11:12 +0200

    Zadanie 6 - deklaracja stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int FALSE = -1;
+    public static final int TABLE_SIZE = 12;
+    private int[] numbers = new int[TABLE_SIZE];
 
-    public int total = -1;
+    public int total = FALSE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +14,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == FALSE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == TABLE_SIZE-1;
     }
 
     protected int peekaBoo() {
         if (callCheck())
-            return -1;
+            return FALSE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return FALSE;
         return numbers[total--];
     }
 
